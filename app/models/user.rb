9dc9152625f2894852a822a53999574732d77217
Hayet Kumasseh <naaakleh@gmail.com>
2022-08-12 09:40:25 -0400

    model modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
 
     has_many :bucket_lists
     has_many :likes
-    has_many :activities, through: :likes
-    # has_many :activities, through: :bucket_lists --no direct link between activities
-    #                                               and bucket list in table 
+    # has_many :activities, through: :likes
+    # has_many :activities, through: :bucket_lists #--no direct link between activities
     #has_many :activities, through: :activity_bucket_lists
     #has_many :activity_bucket_lists, through: :bucket_lists ---redundant?
 end
